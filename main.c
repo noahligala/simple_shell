ff4fceb3fa7731718ddfb5c6446736b9360001f8
noahligala <63068195+noahligala@users.noreply.github.com>
2023-11-16 02:44:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,44 +1,43 @@
 #include "shell.h"
 
-/**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
- *
- * Return: 0 on success, 1 on error
- */
-int main(int ac, char **av)
-{
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+int main(int argc, char **argv) {
+    info_t info[] = { INFO_INIT };
+    int file_descriptor = 2;
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+    // Using inline assembly to manipulate the file descriptor
+    asm ("mov %1, %0\n\t"
+        "add $3, %0"
+        : "=r" (file_descriptor)
+        : "r" (file_descriptor));
 
-	if (ac == 2)
-	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = fd;
-	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+    // Check if there is exactly one command-line argument
+    if (argc == 2) {
+        // Try to open the specified file for reading
+        int fd = open(argv[1], O_RDONLY);
+        if (fd == -1) {
+            // Handle file opening errors
+            if (errno == EACCES) {
+                exit(126);
+            } else if (errno == ENOENT) {
+                _eputs(argv[0]);
+                _eputs(": 0: Can't open ");
+                _eputs(argv[1]);
+                _eputchar('\n');
+                _eputchar(BUF_FLUSH);
+                exit(127);
+            } else {
+                return EXIT_FAILURE;
+            }
+        }
+        info->readfd = fd; // Assign the file descriptor
+    }
+
+    // Initialize the environment list and read command history
+    populate_env_list(info);
+    read_history(info);
+
+    // Execute the shell program
+    hsh(info, argv);
+
+    return EXIT_SUCCESS;
 }
